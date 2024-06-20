52b7d87465685b89f12a7616eb9378859b01f67c
LAB-USR-CHIMBOTE <LAB-USR-CHIMBOTE@UTP394348.grupoutp.net>
2024-06-20 14:09:49 -0500

    ejemplo uso de Neebteans

---
+++ src/main/java/com/mycompany/jemplo/Jemplo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jemplo;
+
+/**
+ *
+ * @author LAB-USR-CHIMBOTE
+ */
+public class Jemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
